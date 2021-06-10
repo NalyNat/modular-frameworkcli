f806b5de3f0af5fba1f457041f82ae3670242b69
NalyNat <79080540+NalyNat@users.noreply.github.com>
2021-06-10 19:37:35 +0000

    Updated to product apple mac ear

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Apple watch";
+		String product = "Apple mac ear";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
